972ebffc8d0b1e44d19ee86bc91dc75e1d40e1c0
Thomas Champagne <th.champagne@gmail.com>
2020-08-30 15:01:00 +0200

    Upgrade try 01

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,14 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
 const libraryName = 'promise-tron'
+
+console.warn(pkg)
 
 export default {
   input: `src/${libraryName}.ts`,
@@ -21,20 +23,22 @@ export default {
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: ['electron'],
   watch: {
-    include: 'src/**'
+    include: './src/*.ts',
   },
   plugins: [
+    // Compile TypeScript files
+    typescript({
+      tsconfig: './tsconfig.json',
+      include: ['./src/*.ts']
+    }),
     // Allow json resolution
     json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
     // Resolve source maps to the original source
     sourceMaps()
   ]
